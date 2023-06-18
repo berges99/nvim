482e8960088458db161e9d9a0d1867ed5f603c58
berges99 <daviid.bergees@gmail.com>
2023-06-18 13:11:02 +0200

    Some housekeeping

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -104,6 +104,11 @@ _G.packer_plugins = {
     path = "/Users/dberges/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
+  ["copilot.vim"] = {
+    loaded = true,
+    path = "/Users/dberges/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    url = "https://github.com/github/copilot.vim"
+  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/Users/dberges/.local/share/nvim/site/pack/packer/start/friendly-snippets",
@@ -179,6 +184,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/dberges/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-oscyank"] = {
+    loaded = true,
+    path = "/Users/dberges/.local/share/nvim/site/pack/packer/start/vim-oscyank",
+    url = "https://github.com/ojroques/vim-oscyank"
   }
 }
 
